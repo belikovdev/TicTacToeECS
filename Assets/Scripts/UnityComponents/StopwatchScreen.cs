babe6b6ccdb5852b3496293a49f09a96a0e03658
belikovdev <belikovdev99@gmail.com>
2021-06-13 14:47:28 +0300

    Add restart button

--- Assets/Scripts/UnityComponents/StopwatchScreen.cs
+++ Assets/Scripts/UnityComponents/StopwatchScreen.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StopwatchScreen : Screen
 {
     public Text text;
+
+    public void OnRestartClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
